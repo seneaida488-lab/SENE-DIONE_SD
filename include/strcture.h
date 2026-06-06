661dcbccf5bd811cf6538e4189f841d7013af037
coura <courandione695@gmail.com>
2026-06-06 15:09:55 +0200

    travail

---
+++ include/strcture.h
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+typedef struct {
+int jour; 
+int mois; 
+int annee;
+}Date;
+typedef struct {
+ int id;
+ char nom[20];   
+}Cours;
+typedef struct {
+int matricule;     
+float valeur;
+char matiere[50]; 
+char *appreciation;
+Date dateDeliberation;
+Cours *cours;;
+int tab[10];
+}Note;
+typedef struct{
+int id;
+char nom[16];
+}Niveau;
+typedef struct {
+int matricule;
+float montant;
+char etat[30];
+char *filliere;
+Date dateInscription;
+Niveau *niveau;
+int tab[20];
+}Inscription;
+typedef struct{
+int matricule;
+float moyenne;
+char nom[40];
+char *prenom;
+Date dateNaissance;
+Note *tabnote;
+Inscription *inscription;	
+}Etudiant;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
